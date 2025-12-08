4b827ac3aaaa0559c5749f93da30fbe5e2c3aa48
PiotrCembrowski <losttape@gmail.com>
2025-12-08 16:54:23 +0100

    setup

---
+++ scripts/main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const DINO_START_POSITION := Vector2i(150, 485)
+const CAM_START_POSITION := Vector2i(576, 324)
+
+var speed : float
+const START_SPEED : float = 10.0
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
