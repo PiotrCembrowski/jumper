98844b25fd1b199f99a51f5f30904e7f11aa250c
PiotrCembrowski <losttape@gmail.com>
2025-12-08 16:39:43 +0100

    working animations for run

--- scripts/dino.gd
+++ scripts/dino.gd
@@ -11,21 +11,25 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$JumpSound.play()
-	
-	else:
-		velocity += get_gravity() * delta
-		$AnimatedSprite2D.play("jump")
+
 
 	if is_on_floor():
-		$AnimatedSprite2D.play("idle")
 		var direction := Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED / 3.5
 			$AnimatedSprite2D.play("run")
+			
+			if direction < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			$AnimatedSprite2D.play("idle")
 
-
-	
+		
+	else:
+		velocity += get_gravity() * delta
+		$AnimatedSprite2D.play("jump")
 
 	move_and_slide()
