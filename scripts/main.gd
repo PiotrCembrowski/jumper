28a9fddc0ba7d95e0fdae4e58b8f863c2de505d9
PiotrCembrowski <losttape@gmail.com>
2025-12-08 20:52:06 +0100

    adjusting platform to speed

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,19 +3,23 @@ extends Node
 const DINO_START_POSITION := Vector2i(350, 150)
 const CAM_START_POSITION := Vector2i(576, 324)
 
-const SPEED : int = 1
+const SPEED : int = 3
 var screen_size : Vector2i
 
 func _ready() -> void:
 	screen_size = get_window().size
 	new_game()
 
-
 func new_game():
 	$Dino.position = DINO_START_POSITION
 	$Dino.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POSITION
-	$Ground.position = Vector2i(0,0)
-
+	$platform.position = Vector2i(0,-250)
+	
 func _process(delta: float) -> void:
 	$Camera2D.position.y -= SPEED
+	print($Camera2D.position.y)
+
+	if $Camera2D.position.y < 0:
+		$platform.position.y -= SPEED
+		print($platform.position.y)
