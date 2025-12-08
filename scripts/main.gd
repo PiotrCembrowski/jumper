73d38794a398fbc88c7e2a6ca5183e887ce85f91
PiotrCembrowski <losttape@gmail.com>
2025-12-08 18:25:49 +0100

    setup

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,13 +3,17 @@ extends Node
 const DINO_START_POSITION := Vector2i(150, 485)
 const CAM_START_POSITION := Vector2i(576, 324)
 
-var speed : float
-const START_SPEED : float = 10.0
+const SPEED : int = 10
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func new_game():
+	$Dino.position = DINO_START_POSITION
+	$Dino.velocity = Vector2i(0,0)
+	$Camera2D.position = CAM_START_POSITION
+	$Ground.position = Vector2i(0,0)
+
 func _process(delta: float) -> void:
 	pass
