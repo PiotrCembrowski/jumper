56dca6c6ae15c259456a3f333dfe7c59962427c0
PiotrCembrowski <losttape@gmail.com>
2025-12-08 16:22:15 +0100

    fixing standing anim

--- scripts/dino.gd
+++ scripts/dino.gd
@@ -6,22 +6,26 @@ const JUMP_VELOCITY : int = -800
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$JumpSound.play()
+	
+	else:
+		velocity += get_gravity() * delta
+		$AnimatedSprite2D.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if is_on_floor():
+		$AnimatedSprite2D.play("idle")
 		var direction := Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED / 3.5
+			$AnimatedSprite2D.play("run")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	
 
 	move_and_slide()
