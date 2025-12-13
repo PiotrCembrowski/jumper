96e62ad78e2170952c1e80208e465f52fa5679c0
PiotrCembrowski <losttape@gmail.com>
2025-12-13 17:49:55 +0100

    adding conditionals and cursor

--- sdl_window.cpp
+++ sdl_window.cpp
@@ -23,6 +23,13 @@ void draw(){
 
 int main(){
     running = true;
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) std::cout << "Failed at SDL_Init()" << std::endl;
+    
+    if(SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer) < 0) std::cout << "Failed at SDL_CreateWindowAndRenderer()" << std::endl;
+
+    SDL_ShowCursor(1);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
+
     while(running){
         update();
         input();
