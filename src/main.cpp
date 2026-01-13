99e12e9a74302772978557b2fdfca0420f9bfdc4
PiotrCembrowski <losttape@gmail.com>
2026-01-13 15:15:14 +0100

    basic mort

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("Dino Jumper", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    SDL_Texture* mortTex = IMG_LoadTexture(renderer, "assets/mort.png");
-    SDL_Texture* bgTex = IMG_LoadTexture(renderer, "assets/background.png");
+    SDL_Texture* mortTex = IMG_LoadTexture(renderer, "assets/img/mort.png");
+    SDL_Texture* bgTex = IMG_LoadTexture(renderer, "assets/img/background/plx-1.png");
 
     Player player(400, 300);
     bool running = true;
